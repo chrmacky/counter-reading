31a8ba12ab76c6cf008f6a8b8fc52661ca796aae
Christoph Lemke <christoph.lemke@stud.fh-luebeck.de>
2019-02-15 23:24:52 +0100

    add the user

---
+++ counter-reading/CRUser.swift
@@ -0,0 +1,15 @@
+//
+//  CRUser.swift
+//  counter-reading
+//
+//  Created by Christoph Lemke on 15.02.19.
+//  Copyright © 2019 Christoph Lemke. All rights reserved.
+//
+
+import Foundation
+public class CRUser{
+    
+    let username: String
+    let userid: Int64
+   
+}
