7a77bc0122d77a7bc1f7126fa668db449eeae6cb
Christoph Lemke <christoph.lemke@stud.fh-luebeck.de>
2019-02-15 23:20:32 +0100

    Initial Commit

---
+++ counter-reading/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  counter-reading
+//
+//  Created by Christoph Lemke on 15.02.19.
+//  Copyright © 2019 Christoph Lemke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
