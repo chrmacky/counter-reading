4d9e7e3428ec479e0f78120f992e4813109bf116
Christoph Lemke <christoph.lemke@stud.fh-luebeck.de>
2019-03-14 10:31:10 +0100

    update mockdata

--- counter-reading/data/CRData_Mockup.swift
+++ counter-reading/data/CRData_Mockup.swift
@@ -16,7 +16,9 @@ class CRData_Mockup {
     var counterList = [CRBaseCounter]()
     var counterOneList = [CRBaseCounter]()
     var locationList = [CRLocation]()
-    var counterValuesList = [CRValues]()
+    var counterValueListWater1 = [CRValues]()
+    var counterValueListWater2 = [CRValues]()
+    var counterValueListStrom1 = [CRValues]()
     
     init(userList: [CRUser], counterList: [CRBaseCounter]){
         self.userList = userList
@@ -24,24 +26,35 @@ class CRData_Mockup {
         loadMockData()
     }
     
+    // load default sample Data
     private func loadMockData(){
-        let counterWater1 = CRBaseCounter(counterid: "TER34", countername: "kalt Wasser", counterStartValue: 12.0, counterCat: .Strom, counterLimitValue: 456.55, counterUnit: .Kilowatt, counterValues: counterValuesList)
-        
-        let counterWater2 = CRBaseCounter(counterid: "FGRT457", countername: "warm Wasser", counterStartValue: 2.0, counterCat: .Wasser, counterLimitValue: 568.8, counterUnit: .qubikmeter, counterValues: counterValuesList)
-        
-        let counterStrom1 = CRBaseCounter(counterid: "STR567ZHzhg", countername: "Strom", counterStartValue: 0.0, counterCat: .Strom, counterLimitValue: 1234.8, counterUnit: .Kilowatt, counterValues: counterValuesList)
-        
+        // Sample Counter reader meter
+        let counterWater1 = CRBaseCounter(counterid: "TER34", countername: "kalt Wasser", counterStartValue: 12.0, counterCat: .Strom, counterLimitValue: 456.55, counterUnit: .Kilowatt, counterValues: counterValueListWater1)
+        let counterWater2 = CRBaseCounter(counterid: "FGRT457", countername: "warm Wasser", counterStartValue: 2.0, counterCat: .Wasser, counterLimitValue: 568.8, counterUnit: .qubikmeter, counterValues: counterValueListWater2)
+        let counterStrom1 = CRBaseCounter(counterid: "STR567ZHzhg", countername: "Strom", counterStartValue: 0.0, counterCat: .Strom, counterLimitValue: 1234.8, counterUnit: .Kilowatt, counterValues: counterValueListStrom1)
+        // Sample Location Data
         let altenholz = CRLocation(locId: "Erc56vTz", locName: "Kiel", locPLZ: 24116, locStreet: "FleetStree", locStreetNumber: "23T", locUser: userOneList)
         let hamburg = CRLocation(locId: "ArZ54Re4FRT", locName: "Hamburh", locPLZ: 23452, locStreet: "Fischmark", locStreetNumber: "45 B", locUser: userList)
+        // Add counter reader meter to list
         counterList.append(counterWater2)
         counterList.append(counterStrom1)
         counterOneList.append(counterWater2)
-
+        // Create sample user with counter
         let user1 = CRUser(username: "user1", userid: "uuid1", useremail: "user1@hth.to", userlocation: [altenholz], usercounter: [counterWater1])
         let user2 = CRUser(username: "user2", userid: "uuid2", useremail: "uswr2@ghht.frt.de",userlocation: [hamburg], usercounter: counterOneList)
+        let user3 = CRUser(username: "user3", userid: "uuid3", useremail: "user3@gt.de", userlocation: [altenholz], usercounter: counterList)
         userList.append(user1)
         userList.append(user2)
         userOneList.append(user2)
-        
+     
+        let value1 = CRValues(valId: "trtr", valDate: "1552554674", valCount: 16.8)
+        let value2 = CRValues(valId: "rree", valDate: "1551855515", valCount: 13.8)
+        let value3 = CRValues(valId: "hhgt", valDate: "1552455115", valCount: 12.8)
+        let value4 = CRValues(valId: "zzte", valDate: "1551455115", valCount: 10.8)
+        let value5 = CRValues(valId: "zghf", valDate: "1551456115", valCount: 14.8)
+        let value6 = CRValues(valId: "adfe", valDate: "1531455115", valCount: 1.8)
+
+        counterValueListWater1.append(contentsOf: [value1, value2, value3, value4])
+        counterValueListWater2.append(contentsOf: [value5, value6])
     }
 }
